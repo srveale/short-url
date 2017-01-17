a35c4251646c383ee1bc9b4b990fa5f035756db3
Scott Veale <srveale42@gmail.com>
2017-01-17 23:14:52 +0000

    implemented redirect function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
+var PORT = process.env.PORT || 8080; // default port 8080
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 var urlDatabase = {
@@ -22,6 +24,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                        urls: urlDatabase };
@@ -35,3 +46,22 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  if (req.params.shortURL == 'undefined'){
+    throw "don't do that";
+  }
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
+function generateRandomString() {
+  //http://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
